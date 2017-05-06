1371cc69749a35b3f834e446519ec969cb0569d0
George Higbie <gehigbie@gmail.com>
2017-05-05 18:01:20 -0700

    Connected JS file to html

---
+++ scripts.js
@@ -0,0 +1,8 @@
+dropdownMenu = () => {
+    let dropdownClick = document.getElementById("dropdownClick");
+    if(dropdownClick.className === "topnav"){
+        dropdownClick.className += " responsive";
+    }else{
+        dropdownClick.className = "topnav";
+    }
+}
